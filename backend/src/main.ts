e44a5334fbe0a7343e8ede15ccac470d4c62585c
ReshetnikovAV89 <andrey.reshetnikov.89@mail.ru>
2026-05-08 12:14:00 +0800

    chore: clean up config usage

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import 'dotenv/config';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  await app.listen(3000);
+
+  await app.listen(port);
 }
+
 bootstrap();
